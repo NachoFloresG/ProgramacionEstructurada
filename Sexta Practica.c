a1be5aa4621a3415c14b6c25ae7e76eb6d30590d
NachoFloresG <60495588+NachoFloresG@users.noreply.github.com>
2020-06-04 21:52:59 -0500

    Add files via upload

---
+++ Sexta Practica.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Ignacio Flores Gallegos 1659301
+
+int main() {
+	int num=0, i=0, suma=0, prom=0;
+	
+	do{	printf("Ingrese sus numeros e ingrese -1 para terminar:");
+		scanf("%d",&num);
+		suma = suma+num;
+		i++;
+	}while(num >=  0);
+	
+	i=i-1;
+	suma=suma+1;
+	prom=suma/i;
+	printf("La suma de los numero es:%d\n",suma); 
+	printf("El pomedio es:%d",prom);
+}
